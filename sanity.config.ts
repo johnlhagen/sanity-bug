f4871da02e222cae41c8854d92ee47c8933f1069
John Hagen <johnhagenmsc@gmail.com>
2025-03-13 16:28:21 +0100

    bug

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,17 +2,51 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {internationalizedArray} from 'sanity-plugin-internationalized-array'
 
-export default defineConfig({
-  name: 'default',
-  title: 'i8n-test',
-
-  projectId: 'trxxiwve',
-  dataset: 'production',
-
-  plugins: [structureTool(), visionTool()],
-
-  schema: {
-    types: schemaTypes,
+export default defineConfig([
+  {
+    name: 'default',
+    title: 'i8n-test',
+    projectId: 'trxxiwve',
+    dataset: 'production',
+    basePath: '/default',
+    plugins: [
+      structureTool(),
+      visionTool(),
+      internationalizedArray({
+        languages: [
+          {id: 'en', title: 'English'},
+          {id: 'fr', title: 'French'},
+        ],
+        defaultLanguages: ['en'],
+        fieldTypes: ['string'],
+      }),
+    ],
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    name: 'another',
+    title: 'i8n-test-another',
+    projectId: 'trxxiwve',
+    dataset: 'production',
+    basePath: '/another',
+    plugins: [
+      structureTool(),
+      visionTool(),
+      //     internationalizedArray({
+      //   languages: [
+      //     {id: 'en', title: 'English'},
+      //     {id: 'fr', title: 'French'},
+      //   ],
+      //   defaultLanguages: ['en'],
+      //   fieldTypes: ['string'],
+      // })
+    ],
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,14 @@
-export const schemaTypes = []
+import {defineField, defineType} from 'sanity'
+
+export const schemaTypes = [
+  defineType({
+    type: 'document',
+    name: 'simpleDoc',
+    fields: [
+      defineField({
+        name: 'greeting',
+        type: 'internationalizedArrayString',
+      }),
+    ],
+  }),
+]
